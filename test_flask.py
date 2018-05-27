347acc9fab2e169ea905d49f283844fd1fe9b727
Snorre Foss Westman <snorrefo@gmail.com>
2018-05-27 11:51:20 +0200

    no message

--- test_flask.py
+++ test_flask.py
@@ -1,7 +1,19 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
+import requests
 
 
-def test_answer():
-    assert func(3) == 4
+def func(url):
+    r = requests.get(url)
+    return r.status_code
+
+
+def test_Tasks():
+    assert func('http://localhost:5000/todo/api/v1.0/tasks') == 200
+
+
+def test_Task():
+    assert func('http://localhost:5000/todo/api/v1.0/tasks/1') == 200
+
+
+if __name__ == '__main__':
+    test_Tasks()
+    test_Task()
