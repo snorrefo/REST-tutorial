3c201ad8780704f3e398985e6b0e71016af90938
Snorre Foss Westman <snorrefo@gmail.com>
2018-05-27 22:13:46 +0200

    no message

--- web/wsgi.py
+++ web/wsgi.py
@@ -3,6 +3,7 @@
 # for running Flask development server with parameters given below, or
 # gunicorn -w 4 -b 127.0.0.1:5001 wsgi:app
 # for running gunicorn uwsgi server with 4 workers.
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-14-04
 
 import flaskr
 
