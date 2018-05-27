28ce1ca387cc5318ffa7e4b4b1534bdeaa9b9634
Snorre Foss Westman <snorrefo@gmail.com>
2018-05-27 16:14:36 +0200

    Remove locker versions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -25,16 +25,32 @@ def create_app():
     def index():
         return "Hello, World!"
 
+    # curl -i http://localhost:5000/todo/api/v1.0/tasks
     @app.route('/todo/api/v1.0/tasks', methods=['GET'])
     def get_tasks():
         return jsonify({'tasks': tasks})
 
+    # curl -i http://localhost:5000/todo/api/v1.0/tasks/3
     @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
     def get_task(task_id):
         task = [task for task in tasks if task['id'] == task_id]
         if len(task) == 0:
             abort(404)
         return jsonify({'task': task[0]})
+
+    # curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
+    @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+    def create_task():
+        if not request.json or 'title' not in request.json:
+            abort(400)
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': request.json['title'],
+            'description': request.json.get('description', ""),
+            'done': False
+        }
+        tasks.append(task)
+        return jsonify({'task': task}), 201
 
     @app.errorhandler(404)
     def not_found(error):
